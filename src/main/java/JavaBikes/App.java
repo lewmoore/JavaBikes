81c5080da64d3f5876022f07a265c3d0c2b23106
Lewis Moore <lewis.moore2707@gmail.com>
2018-07-05 14:41:05 +0100

    Initialise Maven project and add gitignore

---
+++ src/main/java/JavaBikes/App.java
@@ -0,0 +1,13 @@
+package JavaBikes;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
