a60cef00771921c7ed773df742962eee10deeb1d
lewmoore <lewis.moore2707@gmail.com>
2018-08-20 14:33:37 +0100

    Add constructor to DockingStation with bikesArray

--- src/main/java/JavaBikes/DockingStation.java
+++ src/main/java/JavaBikes/DockingStation.java
@@ -5,7 +5,12 @@ import java.util.ArrayList;
 
 public class DockingStation {
 
-    ArrayList<Bike> bikesArray = new ArrayList<>(20);
+    ArrayList<Bike> bikesArray;
+
+    public DockingStation()
+    {
+       this.bikesArray = new ArrayList<>(20);
+    }
 
     public String addBike(Bike bike){
         bikesArray.add(bike);
